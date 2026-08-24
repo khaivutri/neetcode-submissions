b4fa22491540bb87f189e525bf167be2043b9c2c
trikhai <khai.vutri@hcmut.edu.vn>
2026-08-24 15:30:25 +0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.go
@@ -0,0 +1,19 @@
+func isPalindrome(s string) bool {
+	input := strings.TrimSpace(strings.ToLower(s))
+
+	input = strings.Map(func(r rune) rune{
+		if unicode.IsNumber(r) || unicode.IsLetter(r){
+			return r
+		}
+		return -1
+	}, input)
+	left, right := 0, len(input)-1
+	for left < len(s) && right >0 {
+		if input[left] != input[right]{
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
